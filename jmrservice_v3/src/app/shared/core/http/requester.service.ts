b62b99d6d134678df02838a64254a15199f8e435
RikoxCode <levinzimmermann06@gmail.com>
2024-03-12 16:25:52 +0100

    feat; RequestService created and LogService added

--- jmrservice_v3/src/app/shared/core/http/requester.service.ts
+++ jmrservice_v3/src/app/shared/core/http/requester.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import {LogsService} from "../logs.service";
+import Func = jasmine.Func;
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequesterService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private logService: LogsService
+  ) { }
+
+  private ErrorHandler(method: Function):any {
+
+  }
+
+  public GET(): any{
+    const request = this.ErrorHandler(():any => {
+
+    });
+  }
+
 }
---
+++ jmrservice_v3/src/app/shared/core/logs.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogsService {
+
+  isDevelopmentMode: boolean = false
+
+  constructor() { }
+
+  _(message: string, type: string = "normal"){
+    if(this.isDevelopmentMode){
+      switch (type){
+        case "error":
+          console.error(message);
+          break;
+
+        case "warning":
+          console.warn(message);
+          break;
+
+        case "note":
+          console.log(`%c ${message}`, 'color: grey; text-decoration: underline;');
+          break;
+
+        default:
+          console.log(message);
+          break;
+      }
+    }
+  }
+}
