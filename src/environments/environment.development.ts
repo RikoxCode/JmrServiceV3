657fc60bd43aad18c33649b7735d968f980335bd
Netshlife <111433435+RikoxCode@users.noreply.github.com>
2024-05-22 00:28:34 +0200

    fix; useless folder deleted and environment changed to auth.netshlife.dev

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
   apis: {
-    authAPI: 'http://alphaguard.ch/api/v1/auth',
-    avatarAPI: 'http://alphaguard.ch/api/v1/users/',
-    userAPI: 'http://alphaguard.ch/api/v1/users',
-    friendAPI: 'http://alphaguard.ch/api/v1/friends',
-    srcAPI: 'htt://alphaguard.ch/src',
+    authAPI: 'https://auth.netshlife.dev/api/v1/auth',
+    avatarAPI: 'https://auth.netshlife.dev/api/v1/users/',
+    userAPI: 'https://auth.netshlife.dev/api/v1/users',
+    friendAPI: 'https://auth.netshlife.dev/api/v1/friends',
+    srcAPI: 'https://auth.netshlife.dev/src',
     notemetaAPI: 'http://localhost:3000/api/notemeta/',
   },
   developerLogs: true,
