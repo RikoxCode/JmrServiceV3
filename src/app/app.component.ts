d0a398a70f84a637af0d1854077d4bd12b2c823d
Netshlife <111433435+RikoxCode@users.noreply.github.com>
2024-06-22 18:44:40 +0200

    refactor; responsive sidebar.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,22 @@
 import {Component, effect} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
-import {HeaderComponent} from "./shared/components/header/header.component";
 import {MatIcon} from "@angular/material/icon";
 import {FooterComponent} from "./shared/components/footer/footer.component";
 import {ErrorResponseComponent} from "./shared/components/feedback/error-response/error-response.component";
+import {SidebarComponent} from "./shared/components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [
-        CommonModule,
-        RouterOutlet,
-        HeaderComponent,
-        FooterComponent,
-        MatIcon,
-        ErrorResponseComponent,
-    ],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FooterComponent,
+    MatIcon,
+    ErrorResponseComponent,
+    SidebarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: []
